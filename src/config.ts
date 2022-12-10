66b921ef0705332269b002c3d5f1c48460b795a9
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-12-09 21:28:58 -0300

    styles: init tailwind

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Gabriel Rocha';
-export const SITE_DESCRIPTION = 'Portfolio website for Gabriel Forster Rocha!';
+const SITE_TITLE = 'Gabriel Rocha';
+const SITE_DESCRIPTION = 'Portfolio website for Gabriel Forster Rocha!';
+
+export { SITE_TITLE, SITE_DESCRIPTION };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: ["class", '[data-theme="dark"]'],
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			'sans': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+		}
 	},
 	plugins: [],
 }
